639ba7e1c9b254751b320927d20628a8da930db0
Loren McIntyre <loren@gh.mcint.io>
2026-04-19 04:30:47 -0700

    Bump to 0.3.5

--- src/brew_hop_search/__init__.py
+++ src/brew_hop_search/__init__.py
@@ -4,7 +4,7 @@
 """brew-hop-search: fast offline-first Homebrew search."""
 import os
 
-__version__ = "0.3.4"
+__version__ = "0.3.5"
 
 PYPI_URL = "https://pypi.org/project/brew-hop-search/"
 GITHUB_URL = "https://github.com/mcint/brew-hop-search"
